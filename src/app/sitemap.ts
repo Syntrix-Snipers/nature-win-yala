db70b51b6bf3805a9be15828f136b2d3b745b26a
Pasindu Pathum <pathumpasindu41@gmail.com>
2026-01-15 10:20:04 +0530

    indexing request

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,12 +10,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
-    {
-      url: 'https://www.naturewinyala.com',
-      lastModified,
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
     // Explore
     {
       url: 'https://naturewinyala.com/explore',
@@ -23,21 +17,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.9,
     },
-    {
-      url: 'https://www.naturewinyala.com/explore',
-      lastModified,
-      changeFrequency: 'weekly',
-      priority: 0.9,
-    },
     // Book Now
     {
       url: 'https://naturewinyala.com/book-now',
-      lastModified,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.naturewinyala.com/book-now',
       lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
